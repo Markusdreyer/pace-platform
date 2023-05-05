0522af7776d1dc25564eb5b15b8497e35a5ef63b
Markus Dreyer <m.dreyer@amedia.no>
2023-05-05 12:21:32 +0200

    Add backend boilerplate

---
+++ pace-backend/target/debug/deps/websockets-0c0931b6f1b3ebe0.d
@@ -0,0 +1,5 @@
+/Users/madr/Desktop/netlight/pace-app/pace-backend/target/debug/deps/websockets-0c0931b6f1b3ebe0: websockets/src/main.rs
+
+/Users/madr/Desktop/netlight/pace-app/pace-backend/target/debug/deps/websockets-0c0931b6f1b3ebe0.d: websockets/src/main.rs
+
+websockets/src/main.rs:
---
+++ pace-backend/target/debug/websockets.d
@@ -0,0 +1,1 @@
+/Users/madr/Desktop/netlight/pace-app/pace-backend/target/debug/websockets: /Users/madr/Desktop/netlight/pace-app/pace-backend/websockets/src/main.rs
