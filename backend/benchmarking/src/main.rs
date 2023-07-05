f9aab8826f6fd6c93333e290d7ec029a603f445b
Markus Dreyer <m.dreyer@amedia.no>
2023-07-05 13:45:25 +0200

    Update benchmarking

--- backend/benchmarking/src/main.rs
+++ backend/benchmarking/src/main.rs
@@ -1,14 +1,19 @@
-use rand::{Rng, SeedableRng};
+use futures::{SinkExt, StreamExt};
 use serde::{Deserialize, Serialize};
 use serde_json::from_reader;
+use shared::log::configure_log;
+use shared::model::Settings;
+use shared::setup_config;
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
 use tokio::task;
 use tokio::time::sleep;
-use tracing::{error, info};
-use tungstenite::{connect, Message};
+use tokio_tungstenite::{connect_async, tungstenite::Message as TMessage};
+use tracing::{debug, error, info};
+use tungstenite::Message;
 use url::Url;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -25,10 +30,12 @@ struct Coordinates {
     long: f64,
 }
 
-const TOTAL_CLIENTS: usize = 1;
+const TOTAL_CLIENTS: usize = 100;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let config: Settings = setup_config().expect("could not setup config");
+    configure_log(config.log.level);
     let mut handles = vec![];
 
     for i in 0..TOTAL_CLIENTS {
@@ -38,6 +45,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         handles.push(handle);
         sleep(Duration::from_millis(1000)).await; // pause before spawning next client
     }
+
     for handle in handles {
         let res = handle.await?;
         info!("Client finished: {:?}", res)
@@ -49,56 +57,82 @@ async fn simulate_client(client_id: String) -> Result<(), Box<dyn Error + Send>>
     let file = File::open("location_updates.json").expect("could not open file");
     let reader = BufReader::new(file);
     let location_updates: Vec<LocationUpdate> = from_reader(reader).expect("could not parse json");
+
+    let (ws_stream, _) = connect_async(Url::parse("ws://localhost:8080/race/race1").unwrap())
+        .await
+        .expect("Can't connect");
+
+    let (mut write, mut read) = ws_stream.split();
+
+    // Create a channel for sending messages to be written
+    let (write_tx, mut write_rx): (UnboundedSender<TMessage>, UnboundedReceiver<TMessage>) =
+        mpsc::unbounded_channel();
+
+    let mut update_interval = tokio::time::interval(Duration::from_millis(1000));
+
     let mut update_iter = location_updates.iter();
-    let mut rng = rand::rngs::StdRng::from_entropy();
 
-    let (mut socket, response) =
-        connect(Url::parse("ws://localhost:8080/race/race1").unwrap()).expect("Can't connect");
-
-    loop {
-        let now = SystemTime::now();
-        let timestamp = now
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards")
-            .as_secs();
-        let location_update = match update_iter.next() {
-            Some(update) => LocationUpdate {
-                user_id: update.user_id.clone(),
+    let writer = async {
+        while let Some(update) = update_iter.next() {
+            update_interval.tick().await;
+            let now = SystemTime::now();
+            let timestamp = now
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_secs();
+            let location_update = LocationUpdate {
+                user_id: client_id.clone(),
                 timestamp,
                 coordinates: Coordinates {
                     lat: update.coordinates.lat,
                     long: update.coordinates.long,
                 },
-            },
-            None => return Ok(()), // Stop the loop if there are no more updates
-        };
+            };
 
-        // Add randomness to the coordinates
+            let json = serde_json::to_string(&location_update).unwrap();
+            write
+                .send(TMessage::Text(json))
+                .await
+                .expect("could not send message");
+        }
 
-        let location_update = LocationUpdate {
-            user_id: client_id.clone(),
-            timestamp,
-            coordinates: Coordinates {
-                lat: location_update.coordinates.lat,
-                long: location_update.coordinates.long,
-            },
-        };
+        write_tx.send(TMessage::Close(None)).unwrap(); // send close message to reader
 
-        let json = serde_json::to_string(&location_update).unwrap();
-
-        socket.write_message(Message::Text(json)).unwrap();
-
-        match socket.read_message() {
-            Ok(msg) => {
-                if let Message::Ping(ping) = msg {
-                    socket.write_message(Message::Pong(ping)).unwrap();
-                }
-            }
-            Err(e) => {
-                error!("Error reading message: {:?}", e);
+        while let Some(message) = write_rx.recv().await {
+            match message {
+                TMessage::Close(_) => break,
+                _ => write
+                    .send(message)
+                    .await
+                    .map_err(|e| -> Box<dyn Error + Send> { Box::new(e) })?,
             }
         }
 
-        sleep(Duration::from_millis(1000)).await; // pause before sending the next location update
-    }
+        info!("Client finished");
+        Ok::<(), Box<dyn Error + Send>>(())
+    };
+
+    let reader = async {
+        while let Some(msg) = read.next().await {
+            match msg {
+                Ok(Message::Ping(ping)) => {
+                    debug!(message = "Received ping", user_id = client_id);
+                    write_tx.send(Message::Pong(ping)).unwrap();
+                }
+                Ok(Message::Close(_)) => break, // Break the loop when close message is received
+                Ok(_) => {}
+                Err(e) => {
+                    error!("Error reading message: {:?}", e);
+                }
+            }
+        }
+        Ok::<(), Box<dyn Error + Send>>(())
+    };
+
+    tokio::select! {
+        res = writer => res?,
+        res = reader => res?,
+    };
+
+    Ok(())
 }
