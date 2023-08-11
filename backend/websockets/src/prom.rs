dd5173dbeeb469c52ed6e1e79ef3d70f93f63e77
Markus Archer Goulden Dreyer <m.dreyer@amedia.no>
2023-08-11 09:37:06 +0200

    Improve latency metrics (#15)

--- backend/websockets/src/prom.rs
+++ backend/websockets/src/prom.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 use lazy_static::lazy_static;
-use prometheus::{IntCounter, IntGauge, Registry};
+use prometheus::{Histogram, IntCounter, IntGauge, Registry};
 
 lazy_static! {
     pub static ref REGISTRY: Registry = Registry::new();
@@ -16,9 +18,19 @@ lazy_static! {
     pub static ref DISCONNECTED_CLIENTS: IntGauge =
         IntGauge::new("disconnected_clients", "Disconnected Clients")
             .expect("metric can be created");
-    pub static ref LOCATION_UPDATE_LATENCY: IntGauge =
-        IntGauge::new("location_update_latency", "Location Update Latency")
-            .expect("metric can be created");
+    pub static ref LOCATION_UPDATE_LATENCY: Histogram =
+        Histogram::with_opts(prometheus::HistogramOpts {
+            buckets: prometheus::linear_buckets(5.0, 5.0, 400).unwrap(),
+            common_opts: prometheus::Opts {
+                namespace: "websockets".to_string(),
+                subsystem: "location_update".to_string(),
+                name: "latency_milliseconds".to_string(),
+                help: "Location update latency".to_string(),
+                const_labels: HashMap::from([("env".to_string(), "local".to_string())]),
+                variable_labels: vec![],
+            }
+        })
+        .expect("metric can be created");
 }
 
 pub fn register_custom_metrics() {
